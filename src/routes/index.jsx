abaf3efb843d66e9ab28c7d99d4d06ffc48f475a
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-03-06 08:36:02 +0700

    added pages feature

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages";
+import Feature from "../pages/Feature";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/feature" element={<Feature />} />
       </Routes>
     </BrowserRouter>
   );
